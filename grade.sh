2d9ef7a072cd24bb89c63be1e16b1919a856fa49
cacasares <cacasares@ucsd.edu>
2023-02-15 14:00:22 -0800

    update
    update

--- grade.sh
+++ grade.sh
@@ -2,10 +2,16 @@ CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 git clone $1 student-submission
-if [[ -e DocSearch.java]]
-    then echo "DocSearch find"
-else
+
+
+if [[ -e student-submission/ListExamples.java ]]
+then 
+    echo "File found"
+else 
     error
 fi
+cp../ListExamplesTest.java
+
+
 
 echo 'Finished cloning'
