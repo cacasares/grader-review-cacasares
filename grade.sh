8e67589ee578072b85fd9eb12d9b4785c23a0166
cacasares <cacasares@ucsd.edu>
2023-02-15 14:40:26 -0800

    update
    update

--- grade.sh
+++ grade.sh
@@ -17,9 +17,14 @@ javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
 
 java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples > compile.txt
 
-Failure = 'grep "FAILURES" compile.txt'
+Failure = 'grep -c "FAILURES" compile.txt'
 
-if[[ $?== 0]]
+if [[ $?== 0]]
+then 
+    echo "Compiled successfully"
+else 
+    echo "Compile not successful"
+fi 
 
 
 echo 'Finished cloning'
