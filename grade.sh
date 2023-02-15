acdd3f995c0a4c736a6492964d0ce08e0efce9db
cacasares <cacasares@ucsd.edu>
2023-02-15 14:42:59 -0800

    update
    update

--- grade.sh
+++ grade.sh
@@ -19,7 +19,7 @@ java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnit
 
 Failure='grep -c "FAILURES" compile.txt'
 
-if [[ $?== 0 ]]
+if [[ $? == 0 ]]
 then 
     echo "Compiled successfully"
 else 
